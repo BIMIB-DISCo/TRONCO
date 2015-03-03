2408df9666fe9545bac9493b273499adecc594c3
Giulio Caravagna <caravagn@di.unipi.it>
2015-03-04 00:59:20 +0100

    clustering sensitivity oncoprint

--- R/external/clusters.sensitivity.R
+++ R/external/clusters.sensitivity.R
@@ -1,5 +1,5 @@
 
-"cluster.sensitivity" <- function(cluster.map, reference, dataset) {
+"cluster.sensitivity" <- function(cluster.map, reference, stages=NA, file=NA) {
   require(gplots)
   require(colorspace)
   require(pheatmap)
@@ -18,12 +18,12 @@
   
   if(ncol(cluster.map) == 1) stop('No clustering stability for a unique clustering map!')
 
-  if(!reference %in% colnames(data)) 
+  if(!reference %in% colnames(cluster.map)) 
     stop(paste0('The reference cluster specified is not any of: ', 
-                paste(colnames(data), collapse=', '), '.'))
+                paste(colnames(cluster.map), collapse=', '), '.'))
 
-  ref.clust = which(reference == colnames(data))  
-  colnames(cluster.map)[ref.clust] = paste(colnames(cluster.map)[ref.clust],' [ref.]',sep='') 
+  ref.clust = which(reference == colnames(cluster.map))  
+  colnames(cluster.map)[ref.clust] = paste(colnames(cluster.map)[ref.clust],' [reference]',sep='') 
   
   # Transpose data
   cluster.map = t(cluster.map)
@@ -35,18 +35,13 @@
   id = apply(cluster.map, 1, unique)
   id = unique(unlist(id))
 
- 
-
   # Compute the clustering score
   subdata = cluster.map[-ref.clust,]
   refcol = cluster.map[ref.clust,]
   urefcol = unique(refcol)
 
-  score = rep(0,ncol(cluster.map))
+  score = rep(0, ncol(cluster.map))
  
- 
-
-
   for(i in 1:length(urefcol))
   {
     tmp = as.matrix(subdata[,which(refcol==i)]);
@@ -58,47 +53,33 @@
       curr.score = curr.score + (ncol(tmp) - curr.cardinality)/ncol(tmp);			
     }
     
-    score[which(refcol==i)] = 1 - (curr.score/nrow(tmp));
+    score[which(refcol==i)] = 1 - (curr.score/nrow(tmp))
   }
- 
- print(score)
- 
-  
-  # Load stage information
-  stage = sample(4, ncol(cluster.map), rep=T)
-  num.stages = length(unlist(unique(stage)))
-  
+   
   # Create annotations
-  annotation = data.frame(stage = stage, score=score)
-  rownames(annotation) = colnames(cluster.map)
-  
-  
-  # Heatmap settings
-  # layout
-  fontsize_col = 3
-  
-  # colors
-  # col = diverge_hcl(length(id), c=100) # nice: terrain.colors(5)
-  # col= c('#af8dc3', '#f7f7f7', '#7fbf7b')
-  # col = terrain.colors(length(id))
+  cn = colnames(cluster.map)
+  annotation = data.frame(stage = stages[cn,1], sensitivity=score, row.names=cn, stringsAsFactors=FALSE)
+
+  # Create colors 
   col = brewer.pal(n = length(id), name = 'Set1')
+
+  different.stages = sort(unique(annotation$stage))
+  num.stages = length(different.stages)
+  stage.color = append(brewer.pal(n = num.stages, name = 'YlOrRd'), '#FFFFFF')
+  names(stage.color) = append(levels(as.factor(different.stages)), NA)
+
+  score.color = brewer.pal(n = 10, name = 'Blues')
+
+  # Annotation colors
+  annotation_colors = list(stage=stage.color, sensitivity=score.color)
   
-  stage.color = brewer.pal(n = num.stages, name = 'YlOrRd')
-  score.color = brewer.pal(n = 10, name = 'Greys')
+  # Settings
+  main = paste0("Sensitivity of clustering assigment for ", reference,', with respect to clusters detected for ', paste(rownames(subdata), collapse=', ')) 
   
-  # title
-  main= paste("Clustering assignment for subtypes detection (reference ", rownames(cluster.map)[ref.clust],')') 
-  
-  
-  
-  
-  #####
-  annotation_colors = list(stage=stage.color, score=score.color)
-  
-  
+  fontsize_col = 3
+
   pheatmap(cluster.map, 
            scale = "none", 
-           #clustering_distance_rows = "correlation", 
            cluster_col= F, 
            cluster_rows = F, 
            col=col, 
@@ -107,63 +88,17 @@
            fontsize_col=fontsize_col,
            annotation = annotation,
            annotation_colors = annotation_colors,	
-           border_color='darkgray',
+           border_color='lightgray',
            border=T,
            margins=c(10,10),
            cellwidth = 3, 
-           #cellheight = 18,
+           cellheight = 25,
            #legend_labels = unlist(unique(id)),
            legend=F,
            #	legend_breaks=-1:4
            #treeheight_column = 20,
+           filename=file
   )
   
   return(cluster.map)
-  #filename="./results/KO-WT-OV-3way.sig.batch.heatmap.pdf") # name of file output
 }
-
-# mapk3 = read.table('vecchi228/nbs_k3.txt')
-# mapk4 = read.table('vecchi228/nbs_k4.txt')
-# mapk5 = read.table('vecchi228/nbs_k5.txt')
-
-# mapk3 = read.table('oggi228/nbs_k3.txt')
-# mapk4 = read.table('oggi228/nbs_k4.txt')
-# mapk5 = read.table('oggi228/nbs_k5.txt')
-
-# mapk3 = read.table('oggi220/nbs_k3.txt')
-# mapk4 = read.table('oggi220/nbs_k4.txt')
-# mapk5 = read.table('oggi220/nbs_k5.txt')
-
-# mapk3 = read.table('oggi208/nbs_k3.txt')
-# mapk4 = read.table('oggi208/nbs_k4.txt')
-# mapk5 = read.table('oggi208/nbs_k5.txt')
-
-# mapk3 = read.table('fold/nbs_k3.txt')
-# mapk4 = read.table('fold/nbs_k4.txt')
-# mapk5 = read.table('fold/nbs_k5.txt')
-
-# mapk3 = read.table('1mar/nbs_k3.txt')
-# mapk4 = read.table('1mar/nbs_k4.txt')
-# mapk5 = read.table('1mar/nbs_k5.txt')
-
-<<<<<<< HEAD
-#mapk3 = read.table('ok208/nbs_k3.txt')
-#mapk4 = read.table('ok208/nbs_k4.txt')
-#mapk5 = read.table('ok208/nbs_k5.txt')
-#samples = read.table('ok208/sample_id')
-
-#data = data.frame(mapk3, mapk4, mapk5, row.names = samples[,1])
-#colnames(data) = c('K=3', 'K=4', 'K=5')
-
-#cluster.sensitivity(data, reference = 'K=4')
-=======
-# mapk3 = read.table('ok208/nbs_k3.txt')
-# mapk4 = read.table('ok208/nbs_k4.txt')
-# mapk5 = read.table('ok208/nbs_k5.txt')
-# samples = read.table('ok208/sample_id')
-
-# data = data.frame(mapk3, mapk4, mapk5, row.names = samples[,1])
-# colnames(data) = c('K=3', 'K=4', 'K=5')
-
-# cluster.sensitivity(data, reference = 'K=4')
->>>>>>> origin/development
