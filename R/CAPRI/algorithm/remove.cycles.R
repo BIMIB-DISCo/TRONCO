112bda7a3511a571443710de9d9c84dd4ea96f29
danro9685 <daniele@infection.bio.disco.unimib.it>
2015-04-01 17:37:55 +0200

    I love loops!
    I do love them! :-)

--- R/CAPRI/algorithm/remove.cycles.R
+++ R/CAPRI/algorithm/remove.cycles.R
@@ -21,41 +21,41 @@ function( adj.matrix, weights.matrix, not.ordered, hypotheses = NA ) {
     ordered.weights <- vector();
     ordered.edges <- list();
     
-    # evaluate the possible cycles involving atomic events
-    if(length(not.ordered)>0) {
+    # # evaluate the possible cycles involving atomic events
+    # if(length(not.ordered)>0) {
     	
-    		# consider only the edges that were not ordered by temporal priority
-    		curr.edge.pos = 0;
-    		for(i in 1:length(not.ordered)) {
+    		# # consider only the edges that were not ordered by temporal priority
+    		# curr.edge.pos = 0;
+    		# for(i in 1:length(not.ordered)) {
     			
-    			# consider the events i and j
-        		curr.edge = not.ordered[[i]];
-        		curr.edge.i = curr.edge[1,1];
-        		curr.edge.j = curr.edge[2,1];
+    			# # consider the events i and j
+        		# curr.edge = not.ordered[[i]];
+        		# curr.edge.i = curr.edge[1,1];
+        		# curr.edge.j = curr.edge[2,1];
         		
-        		# check if i --> j is a valid edge
-        		if(adj.matrix[curr.edge.i,curr.edge.j]==1) {
-            		ordered.weights = rbind(ordered.weights,weights.matrix[curr.edge.i,curr.edge.j]);
-            		curr.edge.pos = curr.edge.pos + 1;
-            		new.edge <- array(0, c(3,1));
-            		new.edge[1,1] = curr.edge.i;
-            		new.edge[2,1] = curr.edge.j;
-            		ordered.edges[curr.edge.pos] = list(new.edge);
-        		}
+        		# # check if i --> j is a valid edge
+        		# if(adj.matrix[curr.edge.i,curr.edge.j]==1) {
+            		# ordered.weights = rbind(ordered.weights,weights.matrix[curr.edge.i,curr.edge.j]);
+            		# curr.edge.pos = curr.edge.pos + 1;
+            		# new.edge <- array(0, c(3,1));
+            		# new.edge[1,1] = curr.edge.i;
+            		# new.edge[2,1] = curr.edge.j;
+            		# ordered.edges[curr.edge.pos] = list(new.edge);
+        		# }
         		
-        		# check if j --> i is a valid edge
-        		if(adj.matrix[curr.edge.j,curr.edge.i]==1) {
-            		ordered.weights = rbind(ordered.weights,weights.matrix[curr.edge.j,curr.edge.i]);
-            		curr.edge.pos = curr.edge.pos + 1;
-            		new.edge <- array(0, c(2,1));
-            		new.edge[1,1] = curr.edge.j;
-            		new.edge[2,1] = curr.edge.i;
-            		ordered.edges[curr.edge.pos] = list(new.edge);
-        		}
+        		# # check if j --> i is a valid edge
+        		# if(adj.matrix[curr.edge.j,curr.edge.i]==1) {
+            		# ordered.weights = rbind(ordered.weights,weights.matrix[curr.edge.j,curr.edge.i]);
+            		# curr.edge.pos = curr.edge.pos + 1;
+            		# new.edge <- array(0, c(2,1));
+            		# new.edge[1,1] = curr.edge.j;
+            		# new.edge[2,1] = curr.edge.i;
+            		# ordered.edges[curr.edge.pos] = list(new.edge);
+        		# }
         		
-    		}
+    		# }
     		
-    	}
+    	# }
     
     # consider the patterns related the hypotheses
 	if(!is.na(hypotheses[1])) {
@@ -64,8 +64,8 @@ function( adj.matrix, weights.matrix, not.ordered, hypotheses = NA ) {
 		res = hypothesis.evaluate.cycles(hypotheses,adj.matrix,as.patterns(hypotheses),weights.matrix);
 		
 		# of each pattern, compute the involving edges and weights
-		ordered.edges = c(ordered.edges,res$ordered.edges);
-		ordered.weights = c(ordered.weights,res$ordered.weights);
+		# ordered.edges = c(ordered.edges,res$ordered.edges);
+		# ordered.weights = c(ordered.weights,res$ordered.weights);
 		
 		# matomic provides a map from the atomic events to the hypothesis
 		matomic = res$matomic;
@@ -73,6 +73,21 @@ function( adj.matrix, weights.matrix, not.ordered, hypotheses = NA ) {
 		# mhypotheses provides a map from the hypotheses to the atomic events
 		mhypotheses = res$mhypotheses;
 	
+	}
+	
+	# add the edges to be evaluated during the loop removal
+	curr.edge.pos = 0;
+	for(i in 1:nrow(adj.matrix)) {
+		for(j in 1:nrow(adj.matrix)) {
+			if(adj.matrix[i,j]==1) {
+				ordered.weights = rbind(ordered.weights,weights.matrix[i,j]);
+				curr.edge.pos = curr.edge.pos + 1;
+            	new.edge <- array(0, c(2,1));
+            	new.edge[1,1] = i;
+            	new.edge[2,1] = j;
+            	ordered.edges[curr.edge.pos] = list(new.edge);
+			}
+		}
 	}
 	
 	# sort the edges in increasing order of confidence (i.e. the edges with lower pvalue are the most confident)
