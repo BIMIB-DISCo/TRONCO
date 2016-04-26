2a6fd04532297d5894210656871f8366bdbd4e26
luca-dex <luca.desano@gmail.com>
2016-04-26 12:27:51 +0200

    remove indistinguishable events (2)

--- R/prim.algorithm.R
+++ R/prim.algorithm.R
@@ -85,7 +85,7 @@ prim.fit <- function(dataset,
     
     if (length(invalid.events) > 0) {
         for (i in 1:nrow(invalid.events)) {
-            prima.facie.parents$adj.matrix$adj.matrix.acyclic[invalid.events[i, "cause"],invalid.events[i, "effect"]] = 1;
+            # prima.facie.parents$adj.matrix$adj.matrix.acyclic[invalid.events[i, "cause"],invalid.events[i, "effect"]] = 1;
             prima.facie.parents$adj.matrix$adj.matrix.cyclic[invalid.events[i, "cause"],invalid.events[i, "effect"]] = 1;
         }
     }
