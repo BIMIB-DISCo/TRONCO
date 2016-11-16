cbbbaa89548585c406538b64ca8766a7cedc5d3f
luca-dex <luca.desano@gmail.com>
2016-11-16 14:57:21 +0100

    create an env associated with pkg env fix #83

--- R/zzz.R
+++ R/zzz.R
@@ -7,6 +7,10 @@
 #### are made available under the terms of the GNU GPL v3.0
 #### which accompanies this distribution.
 
-.hypotheses.env <- new.env(parent = emptyenv())
+
+.onLoad <- function(libname, pkgname) {
+	hypotheses.env <- new.env(parent = emptyenv())
+	assign('hypotheses.env', hypotheses.env, asNamespace(pkgname))
+}
 
 #### end of file -- as.functions.R
